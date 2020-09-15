ed1e19533dab668343744d0384f253d16f760891
assia <assia.mahboubi@inria.fr>
2020-09-15 22:48:26 +0200

    more clean up in proofs

--- theories/hanson_elem_analysis.v
+++ theories/hanson_elem_analysis.v
@@ -23,31 +23,29 @@ Section AlgCMissing.
 
 Implicit Types x y z : algC.
   
-Lemma root_le_x s x y : (0 < s)%N -> (0 <= x) -> (0 <= y) ->
-                        (x <= y ^+ s) -> ((s.-root x) <= y) .
+Lemma root_le_x (n : nat) x y :
+  (0 < n)%N -> 0 <= x -> 0 <= y -> x <= y ^+ n -> n.-root x <= y.
 Proof.
-move => Hs Hx Hy Hxys.
-suff : (s.-root x) ^+ s <= y ^+ s.
-  rewrite ler_pexpn2r // .
-  by rewrite nnegrE rootC_ge0 // rootCK.
-by rewrite rootCK.
+move => Hn Hx Hy Hxyn.
+have: (n.-root x) ^+ n <= y ^+ n by rewrite rootCK.
+rewrite ler_pexpn2r // nnegrE rootC_ge0 // rootCK.
 Qed.
 
-Lemma root_x_le s x y : (0 < s)%N -> (0 <= x) -> (0 <= y) ->
-                        (x ^+ s <= y) -> (x <= s.-root y) .
+Lemma root_x_le (n : nat) x y :
+  (0 < n)%N -> (0 <= x) -> (0 <= y) -> (x ^+ n <= y) -> (x <= n.-root y).
 Proof.
-move => Hs Hx Hy Hxys.
-suff : x ^+ s <= (s.-root y) ^+ s.
-  rewrite ler_pexpn2r // .
-  by rewrite nnegrE rootC_ge0 // rootCK.
-by rewrite rootCK.
+move => Hn Hx Hy Hxyn.
+have: x ^+ n <= (n.-root y) ^+ n by rewrite rootCK.
+by rewrite ler_pexpn2r // nnegrE rootC_ge0 // rootCK.
 Qed.
 
-Lemma rootC_leq m n x (Hx : 1 <= x) (Hn : (0 < n)%N) (Hmn : (m >= n)%N) : m.-root x <= n.-root x.
+Lemma rootC_leq (m n : nat) x :
+  1 <= x -> (0 < n)%N -> (n <= m)%N -> m.-root x <= n.-root x.
 Proof.
-have Hmgt0 : (0 < m)%N.
-  by rewrite (leq_trans _ Hmn).
-have Heq p s q (Hp : (p <= q)%N) : s.-root x ^+ q = s.-root x ^+ p * s.-root x ^+ (q - p).
+move=> Hx Hn Hmn.
+have Hmgt0 : (0 < m)%N by rewrite (leq_trans _ Hmn).
+have Heq p s q (Hp : (p <= q)%N) :
+  s.-root x ^+ q = s.-root x ^+ p * s.-root x ^+ (q - p).
   by rewrite -GRing.exprD subnKC.
 suff Hm : (m.-root x) ^+ m <= (n.-root x) ^+ m.
   apply: root_x_le => // .
@@ -64,15 +62,13 @@ Qed.
 
 (* Not sure if actually needed in library, but this lemma is helpful
 to prove one_plus_invx_expx below *)
-Lemma le_mrootn_n (m n : nat) : m.+1.-root n.+1%:R <= ratr n.+1%:~R :> algC.
-apply: root_le_x => // .
-* by rewrite ler0n.
-* by rewrite CratrE /= CratrE /= ler0n.
-* rewrite CratrE /= CratrE /=;  apply lter_eexpr => // .
-  by rewrite ler1n.
+Lemma le_mrootn_n (m n : nat) : m.+1.-root n.+1%:R <= n.+1%:R :> algC.
+apply: root_le_x => //; rewrite ?ler0n //.
+by apply lter_eexpr => //; rewrite ler1n.
 Qed.
 
-Lemma prod_root m n x : (0 < m)%N -> (0 < n)%N -> (0 <= x) ->
+
+Lemma prod_root m n x : (0 < m)%N -> (0 < n)%N -> 0 <= x ->
                         (m * n)%N.-root x = m.-root (n.-root x).
 Proof.
 move => Hm Hn Hx.
@@ -88,19 +84,18 @@ Qed.
 
 End AlgCMissing.
 
-Notation "r '%C'" := (ratr r : algC) (at level 8). (* random level *)
+Notation "r '%:C'" := (ratr r : algC) (at level 8). (* random level *)
 
 (* Section presenting the theory of exp_quo, which corresponds to
 taking a rational exponent of a complex algebraic number *)
 Section RationalPower.
 
-
-
-Definition exp_quo r p q := (q.-root (r%C)) ^+ p.
+Definition exp_quo r p q := (q.-root (r%:C)) ^+ p.
 
 Arguments exp_quo r p%nat q%nat : simpl never.
 
-Lemma exp_quo_less r1 r2 p q : (0 < q)%N -> (0 <= r1) -> (0 <= r2) -> (r1 <= r2) -> (exp_quo r1 p q) <= (exp_quo r2 p q).
+Lemma exp_quo_less r1 r2 p q :
+ (0 < q)%N -> 0 <= r1 -> 0 <= r2 -> r1 <= r2 -> exp_quo r1 p q <= exp_quo r2 p q.
 Proof.
 move => Hq H1 H2 Hleq.
 apply: ler_expn2r; last first.
@@ -117,9 +112,8 @@ suff -> : q.-root (ratr 1) = 1 :> algC by rewrite expr1n.
 by rewrite rmorph1 rootC1.
 Qed.
 
-Lemma exp_quo_lessn r1 p1 q1 p2 q2 :
-  (0 < q1)%N -> (0 < q2)%N -> (1 <= r1) ->
-  (p1 * q2 <= p2 * q1)%N ->
+Lemma exp_quo_lessn r1 (p1 q1 p2 q2 : nat) :
+  (0 < q1)%N -> (0 < q2)%N -> 1 <= r1 -> (p1 * q2 <= p2 * q1)%N ->
   (exp_quo r1 p1 q1) <= (exp_quo r1 p2 q2).
 Proof.
 move => Hq1 Hq2 H1r Hle.
@@ -145,16 +139,11 @@ case/orP: H1r.
 Qed.
 
 
-Lemma exp_quo_r_nat r i : (r ^+ i)%C = exp_quo r i 1.
-Proof.
-rewrite /exp_quo root1C.
-by rewrite CratrE /=.
-Qed.
+Lemma exp_quo_r_nat r i : (r ^+ i)%:C = exp_quo r i 1.
+Proof. by rewrite /exp_quo root1C CratrE /=. Qed.
 
-Lemma exp_quo_nat_nat i j : (i ^ j)%N%:R%C = exp_quo (i%N%:Q) j 1.
-Proof.
-by rewrite natrX exp_quo_r_nat.
-Qed.
+Lemma exp_quo_nat_nat i j : (i ^ j)%N%:R%:C = exp_quo (i%N%:Q) j 1.
+Proof. by rewrite natrX exp_quo_r_nat. Qed.
 
 Lemma exp_quo_mult_distr r1 r2 p1 q1 (Hr2 : 0 <= r2) :
   exp_quo r1 p1 q1 * exp_quo r2 p1 q1 = exp_quo (r1 * r2) p1 q1.
@@ -184,16 +173,16 @@ suff: t1 ^+ (q1 * q2)%N = t2 ^+ (q1 * q2)%N.
   + by rewrite nnegrE; apply: exprn_ge0; rewrite rootC_ge0 ?ler0q //.
   + by rewrite nnegrE; apply mulr_ge0; apply: exprn_ge0;
       rewrite rootC_ge0 // ler0q.
-rewrite /t1 /t2 -GRing.exprM mulnDl GRing.exprD.
+rewrite /t1 /t2 -exprM mulnDl exprD.
 have -> : ((p1 * q2 * (q1 * q2)) = (q1 * q2 * (p1 * q2)))%N.
   by rewrite mulnCA [(p1 * q2)%N]mulnC -!mulnA [(p1 * q2)%N]mulnC.
 have -> : ((p2 * q1 * (q1 * q2)) = (q1 * q2 * (p2 * q1)))%N.
   by rewrite mulnC.
-rewrite prod_root ?ler0q // ![in LHS]GRing.exprM rootCK // rootCK // .
-rewrite ![in RHS]GRing.exprMn.
+rewrite prod_root ?ler0q // ![in LHS]exprM rootCK // rootCK // .
+rewrite ![in RHS]exprMn.
 rewrite -!exprM. rewrite [in RHS]mulnC -[(p2 * _)%N]mulnC.
 rewrite [X in (p2* X)%N]mulnC [(p2* (_ * _))%N]mulnCA ![in RHS]exprM !rootCK //.
-rewrite -![in RHS]GRing.exprM. rewrite {1}mulnC; congr (_ * _).
+rewrite -![in RHS]exprM. rewrite {1}mulnC; congr (_ * _).
 by rewrite mulnC.
 Qed.
 
@@ -218,26 +207,17 @@ rewrite 2!exprM rootCK // .
 by rewrite -mulnA -mulnCA 2!exprM rootCK // -!exprM mulnC.
 Qed.
 
-Lemma exp_quo_ge0 r p q :
-  (0 < q)%N ->
-  (0 <= r) ->
-  0 <= exp_quo r p q.
+Lemma exp_quo_ge0 r p q : (0 < q)%N -> 0 <= r -> 0 <= exp_quo r p q.
 Proof.
 by move => Hq Hr; rewrite exprn_gte0 // ?rootC_ge0 // ler0q.
 Qed.
 
-Lemma exp_quo_gt0 r p q :
-  (0 < q)%N ->
-  (0 < r) ->
-  0 < exp_quo r p q.
+Lemma exp_quo_gt0 r p q : (0 < q)%N -> (0 < r) ->  0 < exp_quo r p q.
 Proof.
 by move => Hq Hr; rewrite exprn_gte0 // ?rootC_gt0 // ?ltr0q.
 Qed.
 
-Lemma exp_quo_ge1 r p q :
-  (0 < q)%N ->
-  (1 <= r) ->
-  1 <= exp_quo r p q.
+Lemma exp_quo_ge1 r p q : (0 < q)%N -> (1 <= r) -> 1 <= exp_quo r p q.
 Proof.
 by move => Hq Hr; rewrite exprn_ege1 // ?rootC_ge1 // ler1q.
 Qed.
@@ -251,91 +231,8 @@ Proof.
 by move => Hp Hq Hr; rewrite exprn_egt1 // ?rootC_gt1 // ?ltr1q // -lt0n.
 Qed.
 
-Lemma exp_quo_self_grows (p1 q1 p2 q2 : nat) r1 r2 :
-  (0 < q1)%N ->
-  (0 < q2)%N ->
-  (r1 = p1%:Q / q1%:Q) ->
-  (r2 = p2%:Q / q2%:Q) ->
-  (0 < r1) ->
-  (1 <= r2) ->
-  (r1 <= r2) ->
-  exp_quo r1 p1 q1 <= exp_quo r2 p2 q2.
-Proof.
-move => Hq1 Hq2 Hr1 Hr2 Hr1gt0 Hle1r2 Hle12.
-have Hr1pos : 0 <= r1.
-  exact: ltrW.
-have Hr2pos : 0 <= r2.
-  by rewrite Hr2 divr_ge0 // ?ler0z.
-have Hprodpos : (0 < q1 * q2)%N.
-  by rewrite muln_gt0 Hq1 Hq2.
-have Hleq : (p1 * q2 <= p2 * q1)%N.
-  suff HQ : p1%:Q * q2%:Q <= p2%:Q * q1%:Q.
-  by rewrite -!intrM ler_int in HQ.
-  rewrite -ler_pdivl_mulr ?ltr0z //.
-  rewrite [p2%:~R * _]mulrC -mulrA -ler_pdivr_mull ?ltr0z // mulrC.
-  by move: Hle12; rewrite Hr1 Hr2.
-have -> :
-  exp_quo r2 p2 q2 =
-  exp_quo r2 p1 q1 * exp_quo r2 (p2 * q1 - p1 * q2)%N (q1 * q2)%N.
-  rewrite -exp_quo_plus //.
-  apply: exp_quo_equiv => // .
-    by rewrite !muln_gt0 Hq1 Hq2.
-  rewrite !mulnA.
-  rewrite mulnBl mulnDl mulnBl //.
-  have -> : (p1 * q1 * q2 * q2 = p1 * q2 * q1 * q2)%N.
-    by congr muln; rewrite mulnAC.
-  rewrite subnKC // .
-  by rewrite -mulnA -[X in (_ <= X)%N]mulnA leq_mul.
-have -> : exp_quo r2 p1 q1 = exp_quo r1 p1 q1 * exp_quo (r2 / r1) p1 q1.
-  rewrite exp_quo_mult_distr ?divr_ge0 // .
-  congr exp_quo.
-  rewrite mulrCA divrr ?mulr1 //.
-  exact: unitf_gt0.
-rewrite -{1}[exp_quo _ _ _]mulr1.
-rewrite -mulrA.
-apply: ler_pmul => // ; try rewrite ler01 // .
-  exact: exp_quo_ge0.
-rewrite -[1]mulr1.
-apply: ler_pmul => // ; try rewrite ler01 // .
-  apply: exprn_ege1; rewrite rootC_ge1 //.
-  rewrite rmorphM /= CratrE /=.
-  rewrite ler_pdivl_mulr ?ltr0q ?mul1r //.
-  by rewrite ler_rat.
-apply: exprn_ege1. rewrite rootC_ge1 //.
-suff -> : 1 = ratr (1%N%:Q).
-  by rewrite ler_rat.
-move => t; by rewrite ratr_int.
-Qed.
-
-Lemma sqrtC_exp_quo (r : rat) : sqrtC r%C = exp_quo r 1%N 2%N.
-Proof.
-by rewrite /exp_quo expr1.
-Qed.
-
-(* Unused lemma, but might still be of use somewhere else *)
-Lemma bound_exp_imp_bound_root a b v p (Hp : (0 < p)%N) :
-  0 <= a ->
-  0 <= b  ->
-  a ^+ p <= v <= b ^+ p  ->
-  ratr a <= exp_quo v 1 p <= ratr b.
-Proof.
-move => Hage0 Hbge0 ; move/andP => [Hge Hle].
-have Ha : a%C \is Num.nneg.
-  by rewrite nnegrE ler0q.
-have Hvge0 : (0 <= v).
-  by rewrite (ler_trans _ Hge) // exprn_ge0.
-have Hb : b%C \is Num.nneg.
-  by rewrite nnegrE ler0q.
-have Hprootv : (p.-root (ratr v) : algC) \is Num.nneg.
-  by rewrite nnegrE rootC_ge0 // ler0q.
-apply/andP; split.
-have HgeC : (a ^+ p)%C <= v%C by rewrite ler_rat.
-move: HgeC.
-by rewrite -[ratr v](rootCK Hp) /exp_quo CratrE /= ler_pexpn2r // .
-have HgeC : v%C <= (b ^+ p)%C by rewrite ler_rat.
-move: HgeC.
-rewrite -[ratr v](rootCK Hp) /exp_quo CratrE /= ler_pexpn2r // .
-Qed.
+Lemma sqrtC_exp_quo (r : rat) : sqrtC r%:C = exp_quo r 1%N 2%N.
+Proof. by rewrite /exp_quo expr1. Qed.
 
 End RationalPower.
 
@@ -369,76 +266,71 @@ split; apply/andP; split.
 Qed.
 
 (* Formula for a geometric sum in a field *)
-Lemma geometric_sum (R : numFieldType) n (r : R) (Hr : 1 - r != 0) :
+Lemma geometric_sum (R : numFieldType) n (r : R) (Hr : r != 1) :
   \sum_(i < n) r ^+ i = (1 - r ^+ n) / (1 - r).
 Proof.
 elim: n => [|n Hn].
   by rewrite big_ord0 expr0 subrr mul0r.
 suff: (1 - r ^+ n) / (1 - r) + r ^+ n = (1 - r ^+ n.+1) / (1 - r).
   by rewrite big_ord_recr /= Hn.
-have H1neq0 :  (1 : R) != 0 by exact: GRing.oner_neq0.
-rewrite -[r ^+ n]divr1 GRing.addf_div // !mulr1 !divr1; congr (_ / _).
-rewrite -[- r ^+n]GRing.mulrN1 -addrA -mulrDr.
-have -> : -1 + (1 - r) = - r by rewrite addrA [-1 + 1]addrC subrr sub0r.
-by rewrite mulrN exprSr.
+have den_neq0 : 1 - r != 0 by rewrite subr_eq0 eq_sym.
+rewrite -[r ^+ n]divr1 addf_div ?oner_neq0 // !mulr1 !divr1; congr (_ / _).
+by rewrite mulrBr mulr1 subrKA exprSr.
 Qed.
 
-(* A bound on (1 + 1 / (n+1)) ^ (n+2) *)
-Lemma one_plus_invn_expn (n : nat) : ((1%:Q + (1%:Q / n.+1%:Q)%Q) ^+ n.+2 <= 8%:Q).
+Require Import posnum.
+Lemma posnumSn (R : numDomainType) (n : nat) : 0 < (n.+1%:R : R).
+Proof. by []. Qed.
+
+Canonical nat_posnum R n := PosNum (@posnumSn R n).
+
+(* Bad, bad names *)
+Lemma posnumSz (R : numDomainType) (n : nat) : 0 < (n.+1%:~R : R).
+Proof. by []. Qed.
+
+Canonical int_posnum R n := PosNum (@posnumSz R n).
+
+Lemma posnum_expn (R : numDomainType) (n : nat) (x : {posnum R}) : 0 < x%:num ^+ n.
+Proof. by rewrite exprn_gt0. Qed.
+
+Canonical posum_expn (R : numDomainType) n x := PosNum (@posnum_expn R n x).
+
+Lemma posnum_factn (R : numDomainType) (n : nat) : 0 < ((n `!)%:~R : R).
+Proof. rewrite ltr0z; exact: fact_gt0. Qed.
+
+Canonical posum_factn (R : numDomainType) n := PosNum (@posnum_factn R n).
+
+  
+  (* A bound on (1 + 1 / (n+1)) ^ (n+2) *)
+Lemma one_plus_invn_expn (n : nat) : (1 + n.+1%:~R^-1) ^+ n.+2 <= 8%:Q.
 Proof.
-rewrite exprDn.
-suff H1 :
-  \sum_(i < n.+3) 1%:~R ^+ (n.+2 - i) * (1%:~R / n.+1%:~R)%Q ^+ i *+ 'C(n.+2, i)
-  <= 2%:~R * \sum_(i < n.+2) 1%:~R / ((factorial i)%:~R).                          apply: (ler_trans H1).
-  suff He_leq_4 : (\sum_(i < n.+2) 1%:~R / i`!%:~R) <= 4%:Q.
-    apply: (@ler_trans _ (2%:Q * 4%:Q)) => // .
-    apply: ler_pmul => // .
-    have {1}-> : 0%R = \sum_(i < n.+2) 0%:Q.
-      by rewrite big1.
-    by apply: ler_sum => i _; rewrite divr_ge0 // ler0n.
+have step : (1 + n.+1%:~R^-1) ^+ n.+1 <= \sum_(i < n.+2) (i`!)%:~R ^-1 :> rat.
+  rewrite exprDn; apply: ler_sum => i _;  rewrite expr1n mul1r.
+  suff : n.+1%:~R^-1 ^+ i *+ 'C(n.+1, i) * i`!%:~R <= 1 :> rat.
+    by rewrite -lter_pdivl_mulr ?mul1r // ?ltr0z; apply: fact_gt0. (* issue with manifest pos *)
+  rewrite -mulr_natl mulrC mulrA -natrM mulnC bin_ffact exprVn ler_pdivr_mulr //.
+  rewrite mul1r -natrX ler_nat; exact: ffact_le_expn.
+suff h: (1 + n.+1%:~R^-1 : rat) ^+ n.+2 <= 2%:~R * \sum_(i < n.+2) (i`!)%:~R ^-1.
+  apply: ler_trans h _.
+  suff He_leq_4 : \sum_(i < n.+2) i`!%:~R ^-1 <= 4%:Q.
+    by rewrite -[8%:~R]/(2%:~R* 4%:~R : rat) ler_pmul2l.
   apply: (@ler_trans _ (1 + \sum_(i < n.+1) (2%:Q / 3%:Q) ^+ i)).
-    rewrite big_ord_recl /= fact0.
-    apply: ler_add => // .
-    apply: ler_sum => i _ /= .
-    case: i => i Hi; rewrite /bump /= .
-    rewrite ler_pdivr_mulr; last (rewrite ltr0z; exact: fact_gt0).
-    rewrite -ler_pdivr_mull; first rewrite mulr1.
-      rewrite -GRing.exprVn.
-      have -> : (2%:Q / 3%:Q)^-1 = (3%:Q / 2%:Q) by [].
-      exact: fact_greater_geom.
-    exact: exprn_gt0.
-  rewrite geometric_sum // .
-  suff Hgeom_small (i : nat) : (2%:~R / 3%:~R) ^+ i <= 1%Q.
-    have -> : 4%:Q = 1 + 3%:Q by [].
-    apply: ler_add => // .
-    apply: (@ler_trans _ (1 / (1 - 2%:Q / 3%:Q))) => // .
-    apply: ler_pmul => // .
-      rewrite subr_ge0.
-      exact: Hgeom_small.
-    rewrite ler_sub_addl -{1}[1]add0r.
-    apply: ler_add => // .
-    by rewrite exprn_ge0.
-  by rewrite exprn_ile1 // .
-rewrite -exprDn exprS.
-apply: ler_pmul.
-- by apply: addr_ge0 => //; apply: divr_ge0 => // ; apply: ler0z.
-- rewrite exprn_ge0 // .
-  by apply: addr_ge0 => //; apply: divr_ge0 => // ; apply: ler0z.
-- change 2%:Q with (1%:Q + 1); apply: ler_add => // .
-  rewrite ler_pdivr_mulr.
-    by rewrite mul1r ler_int.
-  exact: ltr0z.
-- rewrite exprDn.
-  apply: ler_sum => i _.
-  rewrite expr1n mul1r.
-rewrite lter_pdivl_mulr ?ltr0n ?fact_gt0 // .
-rewrite -mulr_natl mulrC -mulrA.
-rewrite -natrM bin_ffact.
-rewrite expr_div_n expr1n div1r mulrC ler_pdivr_mulr -natrX ?ltr0n ?expn_gt0 //.
-rewrite mul1r ler_nat.
-exact: ffact_le_expn.
+  - rewrite big_ord_recl /= fact0 ler_add2l /=; apply: ler_sum => [] [i Hi] _.
+    rewrite /bump /=.
+    have -> : (2%:~R / 3%:~R) ^+ i = ((3%:~R / 2%:~R) ^+ i) ^-1 :> rat.
+      by rewrite -exprVn invf_div.
+    rewrite lef_pinv ?posrE ?add1n //; first by exact: fact_greater_geom.
+    by rewrite ltr0z; apply: fact_gt0. (* issue with manifest pos *)
+  - rewrite geometric_sum // -[4%:Q]/(1 + 3%:Q) ler_add2l.
+    have -> : 1 - 2%:~R / 3%:~R = 3%:~R^-1 :> rat by [].
+    by rewrite invrK ler_pimull // ler_subl_addr ler_addl.
+suff majl : (1 + n.+1%:~R^-1) <= 2%:R :> rat.
+  by rewrite exprDn -exprDn exprS; apply: ler_pmul.
+by rewrite -[2%:R]/(1 + 1) ler_add2l invf_le1 // ler1z.
 Qed.
 
+
+(* TODO : clean up, use more ^-1 *)
 (* this proof is very long in big part because of exp_quo *)
 Lemma one_plus_invx_expx (p q : nat) (r : rat) (n : nat) :
   (0 < r) -> (r = p%:Q / q%:Q) -> (exp_quo (1%:Q + 1 / r) p q <= (ratr 9%N%:Q)).
@@ -462,12 +354,12 @@ case/orP: (ger_leVge Hle01 Hle0r) => [H1r|Hr1].
 have := (floorQ_spec r).
 set f := floorQ r => Hfloor.
 have Hfnat : f \is a Znat.
-by rewrite Znat_def; apply: floorQ_ge0; apply: ltrW.
+  by rewrite Znat_def; apply: floorQ_ge0; apply: ltrW.
 move/ZnatP: Hfnat => [] m Hfm.
 case/andP: Hfloor => [Hf1 Hf2].
 apply (@ler_trans _ (ratr 8%N%:Q)); last  by rewrite ler_rat.
 have Hle1m : (1 <= m)%N.
-by rewrite ler1q in H1r; have := (floorQ_ge1 H1r); rewrite -/f Hfm.
+  by rewrite ler1q in H1r; have := (floorQ_ge1 H1r); rewrite -/f Hfm.
 
 have Hfloor_inv : (1%:Q / (f%:Q+1)) < 1%:Q / r <= 1 / f%:Q.
   apply/andP; split.
@@ -501,7 +393,7 @@ suff Hinterm : exp_quo (1%:~R + 1 / r) p q <= exp_quo (1%:Q + 1 / f%:Q) m.+1 1.
   apply: (ler_trans Hinterm).
   rewrite Hfm.
   case: m Hfm Hinterm Hle1m => [| m] Hfm Hinterm // Hle1m.
-  rewrite -exp_quo_r_nat ler_rat.
+  rewrite -exp_quo_r_nat ler_rat mul1r.
   exact: one_plus_invn_expn.
 apply: ler_trans.
   apply (@exp_quo_less _ (1%:~R + 1 / f%:~R)) => // .
@@ -562,30 +454,23 @@ have Helper8 :   (p * m <= q)%N.
 
 have Hfloor_inv : (1%:Q / (f%:Q+1)) < r <= 1 / f%:Q.
   apply/andP; split => // .
-  rewrite ltr_pdivr_mulr // .
-  rewrite -ltr_pdivr_mull ?mulr1 // .
-  apply: (@ler_trans _ (exp_quo (1%:~R + (1 + f%:~R)%Q) p q)).
-    exact: exp_quo_less.
-  apply: (@ler_trans _ (exp_quo (1%:~R + (1 + f%:~R)%Q) 1 m)); last first.
-    apply: (@ler_trans _ (exp_quo (((3 ^ (m.+1))%N)%:Q) 1 m)).
-      apply exp_quo_less => // .
-        exact: ler0n.
-      rewrite -{2}rat1 addrA Hfm -!intrD ler_nat -addnA [(1+m)%N]addnC addn1.
-      exact: (replace_exponential m.+1).
-    rewrite /exp_quo expr1 CratrE /= CratrE /= /= expnS natrM rootCMr // .
-      have -> : 9%:Q%C = ratr (3%:Q) * ratr (3%:Q).
-        by rewrite CratrE /= CratrE /= CratrE /= CratrE /= -natrM.
-      rewrite ler_pmul // .
-      + by rewrite rootC_ge0 // ler0n.
-      + by rewrite rootC_ge0 // ler0n.
-      + case: m Hfm Helper1 Helper8 => [|m]// _ _ _; exact: le_mrootn_n.
-    (* TODO: make a lemma out of this *)
-        * rewrite natrX exprCK // ?CratrE // ler0n //.
-        * by rewrite ler0n.
-
-apply: exp_quo_lessn; first exact: Hq; first exact: Helper1;
-  first exact: Helper7.
-rewrite mul1n; exact: Helper8.
+  by rewrite ltr_pdivr_mulr // -ltr_pdivr_mull ?mulr1.
+apply: (@ler_trans _ (exp_quo (1%:~R + (1 + f%:~R)%Q) p q)).
+  exact: exp_quo_less.
+apply: (@ler_trans _ (exp_quo (1%:~R + (1 + f%:~R)%Q) 1 m)); last first.
+- apply: (@ler_trans _ (exp_quo (((3 ^ (m.+1))%N)%:Q) 1 m)).
+  * apply exp_quo_less => //; first exact: ler0n.
+    rewrite -{2}rat1 addrA Hfm -!intrD ler_nat -addnA [(1+m)%N]addnC addn1.
+    exact: (replace_exponential m.+1).
+  * rewrite /exp_quo expr1 !CratrE /= expnS natrM rootCMr ?ler0n //. 
+    have -> : 9%:R = 3%:R * 3%:R :> algC by rewrite -natrM.
+    rewrite ler_pmul // .
+    + by rewrite rootC_ge0 // ler0n.
+    + by rewrite rootC_ge0 // ler0n.
+    + case: m Hfm Helper1 Helper8 => [|m]// _ _ _; exact: le_mrootn_n.
+      (* TODO: make a lemma out of this *)
+    rewrite natrX exprCK // ?CratrE // ler0n //.
+by apply: exp_quo_lessn=> //; rewrite mul1n. 
 Qed.
 
 End FourFacts.
